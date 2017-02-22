322e74a00114b4be321fc81225d87c4c174dceb6
Vladan Markovic <vladanm@gmail.com>
2017-02-22 08:19:11 +0100

    vers.

--- lib/app/models/magic_attribute.rb
+++ lib/app/models/magic_attribute.rb
@@ -2,9 +2,9 @@
 class MagicAttribute < ActiveRecord::Base
   belongs_to :magic_field
 
-  def self.ransackable_attributes(auth_object = nil)
-    [:value]
-  end
+  #def self.ransackable_attributes(auth_object = nil)
+  #  [:value]
+  #end
 
   def to_s
     value
